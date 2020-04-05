5c2b05811d7559630ffb7a7042da878cb9d22b61
faeschier <63092227+faeschier@users.noreply.github.com>
2020-04-05 11:23:03 +0200

    Update TowerGrosseMachineLaser.java

--- Elements_de_jeu/TowerGrosseMachineLaser.java
+++ Elements_de_jeu/TowerGrosseMachineLaser.java
@@ -2,8 +2,11 @@ package Elements_de_jeu;
 public class TowerGrosseMachineLaser extends Towers {
 
 
-    public TowerGrosseMachineLaser(){
-        super();
+    public TowerGrosseMachineLaser(int x, int y, int buildCost, int buildTime, int attackRange, int speedRotation, int attackSpeed,
+                            int attackDamage, int level, int upgradeCost, int upgradeTime, int sellCost) {
+        super(int x, int y, int buildCost, int buildTime, int attackRange, int speedRotation, int attackSpeed,
+        int attackDamage, int level, int upgradeCost, int upgradeTime, int sellCost);
+        initTurret();
     }
     
      public void initTurret() {
