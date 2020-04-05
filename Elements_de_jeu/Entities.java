b85a46c0f4c4a9a4b592114999845240974865c8
faeschier <63092227+faeschier@users.noreply.github.com>
2020-04-05 11:29:37 +0200

    Update Entities.java

--- Elements_de_jeu/Entities.java
+++ Elements_de_jeu/Entities.java
@@ -1,14 +1,43 @@
 package Elements_de_jeu;
 
+import javafx.scene.image.Image;
+
+import javax.swing.*;
+
 public class Entities {
-    private Position localisation;
+    private int x;
+    private int y;
     private int id;
     private String description;
     private int height;
     private int width;
+    private Image image;
 
-    public String getDescription() {
-        return description;
+    public void getImageDimensions() {
+
+        width = image.getWidth(null);
+        height = image.getHeight(null);
     }
+
+    public void loadImage(String imageName) {
+
+        ImageIcon ii = new ImageIcon(imageName);
+        image = ii.getImage();
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+
+
 }
 
