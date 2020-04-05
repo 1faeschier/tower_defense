9af9b2616699bf5994ccfc12935ef1b80d0b0f52
albanduv <62695999+albanduv@users.noreply.github.com>
2020-04-05 13:51:28 +0200

    Update Enemies.java

--- Elements_de_jeu/Enemies.java
+++ Elements_de_jeu/Enemies.java
@@ -1,18 +1,35 @@
 package Elements_de_jeu;
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import java.util.ArrayList;
 
-public class Enemies implements ShootingObserver{
+public class Ennemie {
+    private int id; //=matricule
+    private int power; //résistance aux tirs
+    private String description; //pour le différencier à l'écran
+    private int speed;//vitesse du perso
+    private static ArrayList<Coordinate> path;
+    public Position position;
+    public int health = 200;
+    public Rectangle r = new Rectangle();
 
-    private int speed;
-    private int attackResistance; //résistance aux tirs
-    private int health; //vie
-    private int scoreValue;
-    private boolean dead;
 
+    public void update(){}
+
+
+    Position getPosition(){
+        return position;
+    }
 
     public void looseHealth(int amount) {
-        if (health > 0) {
-            health -= amount;
+        health -= amount;
+        if (health <= 0){
+            r.setWidth(0);
+            r.setHeight(0);
+            r.setStroke(Color.BROWN);
         }
+        System.out.println(health);
     }
 }
+
