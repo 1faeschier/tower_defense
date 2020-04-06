600377819c44697ba3cc0a3d71b265303aacc73f
faeschier <63092227+faeschier@users.noreply.github.com>
2020-04-06 10:56:13 +0200

    Update Towers.java

--- Elements_de_jeu/Towers.java
+++ Elements_de_jeu/Towers.java
@@ -66,6 +66,93 @@ public class Towers extends Entities implements Runnable, Shooting {
     public ArrayList<Projectile> getProjectiles() {
         return projectiles;
     }
+package Elements_de_jeu;
+import javafx.scene.image.Image;
+
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+public class Towers extends Entities implements Shooting {
+    private int buildCost;
+    private int buildTime;
+    private int attackRange;
+    private int speedRotation;
+    private int attackSpeed;
+    private int attackDamage;
+    private int level;
+    private int upgradeCost;
+    private int upgradeTime;
+    private int sellCost;
+
+
+    public Towers(int buildCost, int buildTime, int attackRange, int speedRotation, int attackSpeed,
+                  int attackDamage, int level, int upgradeCost, int upgradeTime, int sellCost, int id,
+                  String description, int height, int width, Image image){
+        super(id, description, image, height, width);
+        this.buildCost = buildCost;
+        this.buildTime = buildTime;
+        this.attackRange = attackRange;
+        this.attackSpeed = attackSpeed;
+        this.speedRotation = speedRotation;
+        this.attackSpeed = attackSpeed;
+        this.attackDamage = attackDamage;
+        this.level = level;
+        this.upgradeCost = upgradeCost;
+        this.upgradeTime = upgradeTime;
+        this.sellCost = sellCost;
+        initTurret();
+    }
+
+    private ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
+    private ArrayList<Position> positions = new ArrayList<Position>();
+
+    public void shoot(float x, float y){
+    }
+}
+
+    public int getattackDamage() {
+        return attackDamage;
+    }
+
+    public int getBuildCost() {
+        return buildCost;
+    }
+
+    public void upgrade(int score) {
+        if (upgradeCost <= score) {
+            if (MouseEvent ==) {
+                score -= upgradeCost;
+                upgradeCost += 20;
+                attackDamage += 20;
+                sellCost += 20;
+                attackRange += 10;
+                attackSpeed += 5;
+                upgradeTime += 50;
+                sellCost += 20;
+                level = level + 1;
+            } else if {
+                return ("Tu n'as pas les fonds nécessaires, retourne shooter des méchants")
+            }
+        }
+    }
+    public ArrayList<Projectile> getProjectiles() {
+        return projectiles;
+    }
+
+    public void initTurret() {
+        loadImage("src/turr.png");
+        getImageDimensions();
+    }
+
+
+    public void addPosition(Position p){
+        positions.add(p);
+    }
+
+    public ArrayList<Position> getPositionsList(){
+        return positions;
+    }
+}
 
     public void initTurret() {
         loadImage("src/turr.png");
