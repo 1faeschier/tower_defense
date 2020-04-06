17fe1ea300a5cf5f455a506d4e8d66860fca3a5a
faeschier <63092227+faeschier@users.noreply.github.com>
2020-04-06 10:54:15 +0200

    Update TowerGrosseMachineLaser.java

--- Elements_de_jeu/TowerGrosseMachineLaser.java
+++ Elements_de_jeu/TowerGrosseMachineLaser.java
@@ -1,16 +1,15 @@
 package Elements_de_jeu;
+
+import javafx.scene.image.Image;
+
 public class TowerGrosseMachineLaser extends Towers {
 
+    public TowerGrosseMachineLaser(int buildCost, int buildTime, int attackRange, int speedRotation, int attackSpeed,
+                                   int attackDamage, int level, int upgradeCost, int upgradeTime, int sellCost, int id,
+                                   String description, int height, int width, Image image){
+        super ( buildCost, buildTime, attackRange, speedRotation, attackSpeed, attackDamage,  level,  upgradeCost,
+                upgradeTime, sellCost, id, description, height, width, image);
 
-    public TowerGrosseMachineLaser(int x, int y, int buildCost, int buildTime, int attackRange, int speedRotation, int attackSpeed,
-                            int attackDamage, int level, int upgradeCost, int upgradeTime, int sellCost) {
-        super(int x, int y, int buildCost, int buildTime, int attackRange, int speedRotation, int attackSpeed,
-        int attackDamage, int level, int upgradeCost, int upgradeTime, int sellCost);
-        initTurret();
-    }
-    
-     public void initTurret() {
-        loadImage("src/turr.png");
-        getImageDimensions();
     }
 }
+
