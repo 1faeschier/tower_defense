215609090fa15b93e9dc70824046d4a2afe20a6a
albanduv <62695999+albanduv@users.noreply.github.com>
2020-04-09 20:53:08 +0200

    Update Towers.java

--- Elements_de_jeu/Towers.java
+++ Elements_de_jeu/Towers.java
@@ -1,11 +1,15 @@
-package Elements_de_jeu;
-import java.awt.event.MouseEvent;
-import java.util.ArrayList;
+package sample;
 
-public class Tower extends Entities implements Runnable, Shooting {
-    private Position position = new Position(500, 130);
-    private int attackRange = 100;
-    private int attackDamage = 5;
+import java.awt.*;
+import java.util.ArrayList;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+
+public class Tower extends Entities implements Runnable {
+    private Position position;
+    private int attackRange = 200;
+    private int attackDamage = 3;
     private int attackSpeed = 100;
     private Thread t;
     private int buildCost;
@@ -15,35 +19,15 @@ public class Tower extends Entities implements Runnable, Shooting {
     private int upgradeCost;
     private int upgradeTime;
     private int sellCost;
-    private Ennemie ennemie;
+    public ArrayList<Ennemie> listenemie;
 
-    public void Tower(int buildCost, int buildTime, int attackRange, int speedRotation, int attackSpeed, int attackDamage, int level, int upgradeCost, int upgradeTime, int sellCost){
-        this.buildCost = buildCost;
-        this.buildTime = buildTime;
-        this.attackRange = attackRange;
-        this.attackSpeed = attackSpeed;
-        this.speedRotation = speedRotation;
-        this.attackDamage = attackDamage;
-        this.level = level;
-        this.upgradeCost = upgradeCost;
-        this.upgradeTime = upgradeTime;
-        this.sellCost = sellCost;
+
+    public Tower(Position position){
+        this.position = position;
         t = new Thread(this);
-        t.start();
     }
 
-
-    public void shoot(float x, float y){
-    }
-}
-
-    public int getattackDamage() {
-        return attackDamage;
-    }
-
-    public int getBuildCost() {
-        return buildCost;
-    }
+    public Position getPosition(){ return position; }
 
     public String upgrade(int score){
         String res = "";
@@ -63,124 +47,34 @@ public class Tower extends Entities implements Runnable, Shooting {
         return res;
     }
 
-    public ArrayList<Projectile> getProjectiles() {
-        return projectiles;
-    }
-package Elements_de_jeu;
-import javafx.scene.image.Image;
-
-import java.awt.event.MouseEvent;
-import java.util.ArrayList;
-
-public class Tower extends Entities implements Shooting {
-    private int buildCost;
-    private int buildTime;
-    private int attackRange;
-    private int speedRotation;
-    private int attackSpeed;
-    private int attackDamage;
-    private int level;
-    private int upgradeCost;
-    private int upgradeTime;
-    private int sellCost;
-
-
-    public Tower(int buildCost, int buildTime, int attackRange, int speedRotation, int attackSpeed,
-                  int attackDamage, int level, int upgradeCost, int upgradeTime, int sellCost, int id,
-                  String description, int height, int width, Image image){
-        super(id, description, image, height, width);
-        this.buildCost = buildCost;
-        this.buildTime = buildTime;
-        this.attackRange = attackRange;
-        this.attackSpeed = attackSpeed;
-        this.speedRotation = speedRotation;
-        this.attackSpeed = attackSpeed;
-        this.attackDamage = attackDamage;
-        this.level = level;
-        this.upgradeCost = upgradeCost;
-        this.upgradeTime = upgradeTime;
-        this.sellCost = sellCost;
-        initTurret();
-    }
-
-    private ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
-    private ArrayList<Position> positions = new ArrayList<Position>();
-
-    public void shoot(float x, float y){
-    }
-}
-
-    public int getattackDamage() {
-        return attackDamage;
-    }
-
-    public int getBuildCost() {
-        return buildCost;
-    }
-
-    public void upgrade(int score) {
-        if (upgradeCost <= score) {
-            if (MouseEvent ==) {
-                score -= upgradeCost;
-                upgradeCost += 20;
-                attackDamage += 20;
-                sellCost += 20;
-                attackRange += 10;
-                attackSpeed += 5;
-                upgradeTime += 50;
-                sellCost += 20;
-                level = level + 1;
-            } else if {
-                return ("Tu n'as pas les fonds nécessaires, retourne shooter des méchants")
-            }
-        }
-    }
-    public ArrayList<Projectile> getProjectiles() {
-        return projectiles;
-    }
-
-    public void initTurret() {
-        loadImage("src/turr.png");
-        getImageDimensions();
-    }
-
-
-    public void addPosition(Position p){
-        positions.add(p);
-    }
-
-    public ArrayList<Position> getPositionsList(){
-        return positions;
-    }
-}
-
-    public void initTurret() {
-        loadImage("src/turr.png");
-        getImageDimensions();
-    }
-
     public Rectangle getForme(){
         Rectangle rectangle = new Rectangle();
         rectangle.setHeight(50); rectangle.setWidth(50); rectangle.setFill(Color.WHITE); rectangle.setStroke(Color.BLACK);
-        rectangle.setX(500); rectangle.setY(130);
+        rectangle.setX(position.getX()); rectangle.setY(position.getY());
         return rectangle;
     }
+
+
+    public void shoot(float x, float y){
+    }
+
     @Override
     public void run() {
-        while(true) {
-            if (position.distance(listenemie.get(0).getPosition()) < attackRange){
-                try {
-                    listenemie.get(0).looseHealth(attackDamage);
-                    Thread.sleep(attackSpeed);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            else{
-                try {
-                    Thread.sleep(attackSpeed);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        for (Ennemie ennemie : listenemie) {
+            while (ennemie.getHealth() > 0) {
+                if (position.distance(ennemie.getPosition()) < attackRange) {
+                    try {
+                        ennemie.looseHealth(attackDamage);
+                        Thread.sleep(attackSpeed);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    try {
+                        Thread.sleep(attackSpeed);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
