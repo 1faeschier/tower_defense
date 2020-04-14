cca8e9e0c2bf80be8ce553fc052301e0cbd4c3ce
albanduv <62695999+albanduv@users.noreply.github.com>
2020-04-14 21:35:23 +0200

    Update Towers.java

--- Elements_de_jeu/Towers.java
+++ Elements_de_jeu/Towers.java
@@ -2,18 +2,19 @@ package sample;
 
 import java.awt.*;
 import java.util.ArrayList;
+
+import javafx.animation.Timeline;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.Line;
-import java.util.ArrayList;
+import javafx.scene.transform.Rotate;
 
 
-
-public class Tower implements Runnable {
+public class Tower extends Entities implements Runnable {
     private Position position;
     private int attackRange = 200;
-    private int attackDamage = 15;
-    private int attackSpeed = 100;
+    private int attackDamage = 24;
+    private int attackSpeed = 400;
     private Thread t;
     private int buildCost;
     private int buildTime;
@@ -23,11 +24,15 @@ public class Tower implements Runnable {
     private int upgradeTime;
     private int sellCost;
     public ArrayList<Ennemie> listenemie;
+    public Polygon triangle = new Polygon();
+    private double angle;
+    private Ennemie e = null;
 
 
     public Tower(Position position){
         this.position = position;
         t = new Thread(this);
+        angle = 0;
     }
 
     public Position getPosition(){ return position; }
@@ -42,7 +47,7 @@ public class Tower implements Runnable {
             attackRange += 10;
             attackSpeed += 5;
             upgradeTime += 50;
-            res = "amérlioratioin réalisé avec succès !";
+            res = "amérlioration réalisée avec succès !";
         }
         else {
             res =  "Tu n'as pas les fonds nécessaires, retourne shooter des méchants";
@@ -50,11 +55,19 @@ public class Tower implements Runnable {
         return res;
     }
 
-    public Rectangle getForme(){
-        Rectangle rectangle = new Rectangle();
-        rectangle.setHeight(50); rectangle.setWidth(50); rectangle.setFill(Color.WHITE); rectangle.setStroke(Color.BLACK);
-        rectangle.setX(position.getX()); rectangle.setY(position.getY());
-        return rectangle;
+    public Polygon getForme(){
+        double val = Math.sqrt(3)/6;
+        triangle.getPoints().setAll(
+                position.getX(), position.getY(),
+                position.getX()-50*Math.sin(((30+angle)/180)*Math.PI), position.getY()+50*Math.cos(((30+angle)/180)*Math.PI),
+                position.getX()+50*Math.sin(((30-angle)/180)*Math.PI), position.getY()+50*Math.cos(((30-angle)/180)*Math.PI)
+        );
+        triangle.setFill(Color.WHITE); triangle.setStroke(Color.BLACK);
+        return triangle;
+    }
+
+
+    public void shoot(float x, float y){
     }
 
     @Override
@@ -68,10 +81,11 @@ public class Tower implements Runnable {
                 }
             }
             while (ennemie.getHealth() > 0 && position.distance(ennemie.getPosition()) < attackRange) {
+                e = ennemie;
                 if (position.distance(ennemie.getPosition()) < attackRange) {
                     try {
                         ennemie.looseHealth(attackDamage);
-                        System.out.println(ennemie.getHealth());
+                        //System.out.println(ennemie.getHealth());
                         Thread.sleep(attackSpeed);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
@@ -86,9 +100,20 @@ public class Tower implements Runnable {
             }
         }
     }
-
     public void start(ArrayList<Ennemie> list) {
         listenemie = list;
         t.start();
+    }
+
+
+    public void update() {
+        if (e != null){
+        angle = position.getAngle(e.getPosition());}
+
+        triangle.getPoints().setAll(
+                position.getX(), position.getY(),
+                position.getX()-50*Math.sin(((30+angle)/180)*Math.PI), position.getY()+50*Math.cos(((30+angle)/180)*Math.PI),
+                position.getX()+50*Math.sin(((30-angle)/180)*Math.PI), position.getY()+50*Math.cos(((30-angle)/180)*Math.PI)
+        );
     }
 }
