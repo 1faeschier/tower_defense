986881e0a2af01ca21fefa96dd1d1e314f194a5e
albanduv <62695999+albanduv@users.noreply.github.com>
2020-04-14 07:45:25 +0200

    Update Enemies.java

--- Elements_de_jeu/Enemies.java
+++ Elements_de_jeu/Enemies.java
@@ -1,22 +1,31 @@
-package Elements_de_jeu;
+package sample;
 
+import javafx.scene.Node;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import java.util.ArrayList;
 
-public class Ennemie {
+public class Ennemie extends Entities{
     private int id; //=matricule
     private int power; //résistance aux tirs
     private String description; //pour le différencier à l'écran
     private int speed;//vitesse du perso
+    private static ArrayList<Coordinate> path;
     public Position position;
-    public int health = 200;
+    public int health;
     public Rectangle r = new Rectangle();
 
     public int getHealth(){return health;};
-    
+
     public void update(){}
-    
+
+    Position getPosition(){
+        return position;
+    }
+
+    public void looseHealth(int amount) {
+    }
+
     public Rectangle getForme() {
         r.setHeight(20);
         r.setWidth(20);
@@ -25,24 +34,8 @@ public class Ennemie {
         r.setFill(Color.BLUE);
         r.setStroke(Color.BLACK);
         return r;
-    }    
-
-    Position getPosition(){
-        return position;
     }
 
-    public void looseHealth(int amount) {
-        health -= amount;
-        if (health <= 0){
-            r.setWidth(0);
-            r.setHeight(0);
-            r.setStroke(Color.BROWN);
-        }
-        System.out.println(health);
+    public void start(ArrayList<Ennemie> listenemie, Way way, int map) {
     }
-    
-    public void start(ArrayList<Ennemie> listenemie, Way way) {
-    }    
-    
 }
-
