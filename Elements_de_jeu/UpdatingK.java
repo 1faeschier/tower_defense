f5e82d240bdf1b9e48722a79495e63ef5af560a0
faeschier <63092227+faeschier@users.noreply.github.com>
2020-04-24 15:56:44 +0200

    Add files via upload

---
+++ Elements_de_jeu/UpdatingK.java
@@ -0,0 +1,7 @@
+package sample;
+
+import java.util.ArrayList;
+
+public interface UpdatingK {
+    void update(ArrayList<Ennemie> list);
+}
---
+++ Elements_de_jeu/UpdatingS.java
@@ -0,0 +1,5 @@
+package sample;
+
+public interface UpdatingS {
+    void update();
+}
